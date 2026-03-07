5c8a9d2bde90e06236c759998e09cca8eea37f52
Vaishnavi Gattu <vaishnavigattu2096@gmail.com>
2026-03-07 18:50:54 +0530

    Update tasks.py

--- app/api/v1/tasks.py
+++ app/api/v1/tasks.py
@@ -6,15 +6,42 @@ from app.models.models import Task
 
 router = APIRouter()
 
-@router.post('/', response_model=TaskRead)
-def create_task(payload: TaskCreate, session: Session = Depends(get_session)):
-    task = Task(title=payload.title, description=payload.description, user_id=1)
+@router.get('/')
+def list_tasks(session: Session = Depends(get_session)):
+    tasks =  session.exec(select(Task)).all()
+    return tasks
+
+@router.post('/')
+def create_task(task: Task, session: Session = Depends(get_session)):
     session.add(task)
     session.commit()
     session.refresh(task)
     return task
 
-@router.get('/')
-def list_tasks(session: Session = Depends(get_session)):
-    return session.exec(select(Task)).all()
+@router.get("/{task_id}")
+def get_task(task_id: int, session: Session = Depends(get_session)):
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
 
+@router.put("/{task_id}")
+def update_task(task_id: int, updated: Task, session: Session = Depends(get_session)):
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    task.title = updated.title
+    task.description = updated.description
+    session.add(task)
+    session.commit()
+    session.refresh(task)
+    return task
+
+@router.delete("/{task_id}")
+def delete_task(task_id: int, session: Session = Depends(get_session)):
+    task = session.get(Task, task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    session.delete(task)
+    session.commit()
+    return {"delete": task_id}
